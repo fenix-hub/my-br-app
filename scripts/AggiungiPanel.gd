51f00234430820e08b888e1a3612815974d40b62
Nicolò <n.santilio97@gmail.com>
2019-08-11 20:25:06 +0200

    Signed-off-by: Nicolò <n.santilio97@gmail.com>

--- scripts/AggiungiPanel.gd
+++ scripts/AggiungiPanel.gd
@@ -8,21 +8,6 @@ func _ready():
 func registra_prenotazione(nome, cognome, persone, telefono, mail, giorno, mese, anno, ora, minuti,note) -> void:
 	ElencoPrenotazioni.nuova_prenotazione(0, nome, cognome, persone, telefono, mail, giorno, mese, anno, ora, minuti,note)
 
-func registra_cliente_daprenot(nome, cognome, telefono, mail) -> void:
-	var nuovo = false
-	if !ElencoClienti.elenco.empty():
-		for cl in ElencoClienti.elenco:
-			if cl.nome.to_lower() == nome.to_lower() and cl.cognome.to_lower() == cognome.to_lower():
-				ElencoClienti.modifica_cliente(cl.CODICE,cl.nome,cl.cognome,cl.telefono,cl.mail,cl.ragione_sociale,cl.indirizzo,cl.cap,cl.citta,cl.provincia,cl.piva,cl.allergie,cl.note,1)
-				nuovo = false
-				break
-			else:
-				nuovo = true
-	else:
-		ElencoClienti.nuovo_cliente(nome,cognome,telefono,mail,"","","","","","","","",1)
-	if nuovo:
-		ElencoClienti.nuovo_cliente(nome,cognome,telefono,mail,"","","","","","","","",1)
-
 func svuota_campi() -> void:
 	for campo in dati_prenotazione :
 		campo.set_text("")
@@ -38,7 +23,6 @@ func _on_Registra_pressed():
 		prenot.append(campo.get_text())
 	if prenot[0]!= "" or prenot[1]!= "" and prenot[9]!= "" and prenot[3]!= "" and prenot[10]!= "" and prenot[5]!= "" and prenot[6]!= "" and prenot[7]!= "" and prenot[8]!= "":
 		registra_prenotazione(prenot[0],prenot[1],int(prenot[2]),prenot[3],prenot[4],int(prenot[5]),int(prenot[6]),int(prenot[7]),int(prenot[8]),int(prenot[9]),prenot[10])
-		registra_cliente_daprenot(prenot[0],prenot[1],prenot[3],prenot[4])
 		svuota_campi()
 	else:
 		print("Mancano campi")
--- scripts/ModificaPrenotazione.gd
+++ scripts/ModificaPrenotazione.gd
@@ -10,21 +10,6 @@ func _ready():
 func modifica_prenotazione(co, tv, nome, cognome, persone, telefono, mail, giorno, mese, anno, ora, minuti,note) -> void:
 	ElencoPrenotazioni.modifica_prenotazione(co, tv, nome, cognome, persone, telefono, mail, giorno, mese, anno, ora, minuti,note)
 
-func registra_cliente_daprenot(nome, cognome, telefono, mail) -> void:
-	var nuovo = false
-	if !ElencoClienti.elenco.empty():
-		for cl in ElencoClienti.elenco:
-			if cl.nome.to_lower() == nome.to_lower() and cl.cognome.to_lower() == cognome.to_lower():
-				ElencoClienti.modifica_cliente(cl.CODICE,cl.nome,cl.cognome,cl.telefono,cl.mail,cl.ragione_sociale,cl.indirizzo,cl.cap,cl.citta,cl.provincia,cl.piva,cl.allergie,cl.note,0)
-				nuovo = false
-				break
-			else:
-				nuovo = true
-	else:
-		ElencoClienti.nuovo_cliente(nome,cognome,telefono,mail,"","","","","","","","",1)
-	if nuovo:
-		ElencoClienti.nuovo_cliente(nome,cognome,telefono,mail,"","","","","","","","",1)
-
 func svuota_campi() -> void:
 	for campo in dati_prenotazione :
 		campo.set_text("")
@@ -59,7 +44,6 @@ func _on_Modifica_pressed():
 	print(prenot)
 	if prenot[0]!= "" or prenot[1]!= "" and prenot[9]!= "" and prenot[3]!= "" and prenot[10]!= "" and prenot[5]!= "" and prenot[6]!= "" and prenot[7]!= "" and prenot[8]!= "":
 		modifica_prenotazione(prenotaz.CODICE, prenotaz.tavolo, prenot[0], prenot[1],int(prenot[2]),prenot[3],prenot[4],int(prenot[5]),int(prenot[6]),int(prenot[7]),int(prenot[8]),int(prenot[9]),prenot[10])
-		registra_cliente_daprenot(prenot[0],prenot[1],prenot[2],prenot[3])
 		svuota_campi()
 		hide()
 	else:
